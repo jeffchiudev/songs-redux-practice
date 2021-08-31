002886b95f39c95be2a947d9b2bb983001589565
Jeff Chiu <jwchiu82@gmail.com>
2021-08-30 20:09:46 -0700

    add song and selectedSong reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "She's a Rainbow", duration: "4:34" },
+    { title: "Space Oddity", duration: "4:42" },
+    { title: "Mr. Blue Sky", duration: "5:03" },
+    { title: "Don't Stop Me Now", duration: "3:29" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
